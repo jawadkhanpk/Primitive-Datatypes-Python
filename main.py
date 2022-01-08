80a65060d8ec94f5e57427273be924487018ea6e
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-08 21:38:42 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+#Datatypes
+
+
+#Strings
+
+# print("Hello"[4])
+
+#Integers
+
+# print(123 + 456)
+
+123456789
+
+#Float
+
+
+# 3.14159
+
+
+#Boolean
+
+# True
+# False
+#
+# num_char = len(input("What is your name"))
+#
+# new_num_char = str(num_char)
+# print(type(num_char))
+
+# a = float(123)
+# print(type(a))
+
+two_digit_number = input("type a two digit number: ")
+
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+# print(first_digit)
+# print(second_digit)
+
+result = int(first_digit) + int(second_digit)
+print(result)
